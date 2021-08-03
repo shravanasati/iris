cd15e89dc772fa642e52c63d8a9b38526a38f538
Shravan Asati <dev.shravan@protonmail.com>
2021-08-03 14:37:17 +0530

    🐛 fixed windows installation script

--- scripts/windows_install.ps1
+++ scripts/windows_install.ps1
@@ -5,19 +5,48 @@ $url = "https://github.com/Shravan-1908/iris/releases/latest/download/iris-windo
 $dir = $env:USERPROFILE + "\.iris"
 $filepath = $env:USERPROFILE + "\.iris\iris.exe"
 
-[System.IO.Directory]::CreateDirectory($dir)
-(Invoke-WebRequest -Uri $url -OutFile $filepath)
+try {
+    [System.IO.Directory]::CreateDirectory($dir)
+}
+catch {
+    Write-Host "Failed to create directory!"
+    [Environment]::Exit(1)
+}
 
-Write-Host "Adding iris to PATH..."
-[Environment]::SetEnvironmentVariable(
-    "Path",
-    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";"+$dir,
-    [EnvironmentVariableTarget]::Machine)
 
-Write-Host "Adding iris to startup applications..."
+try {
+    (Invoke-WebRequest -Uri $url -OutFile $filepath)
+}
+catch {
+    Write-Host "Failed to download the executable file! Make sure youve active internet connection."
+    [Environment]::Exit(1)
+}
 
-$startupdir = $env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+try {
+    Write-Host "Adding iris to PATH..."
+    [Environment]::SetEnvironmentVariable(
+        "Path",
+        [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::Machine) + ";"+$dir,
+        [EnvironmentVariableTarget]::Machine)
+}
+catch {
+    Write-Host "Failed to add iris to PATH! Make sure you've opened powershell as Admin."
+    [Environment]::Exit(1)
+}
 
-Write-Host "iris" >> $startupdir + "\iris.bat"
+try {
+    Write-Host "Adding iris to startup applications..."
 
-Write-Host "iris installation is successfull!"
+    $startuppath = $env:USERPROFILE + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\iris.lnk"
+
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($startuppath)
+    $Shortcut.TargetPath = $filepath
+    $Shortcut.Save()
+}
+catch {
+    Write-Host "Failed to add iris to startup applications! Make sure you've opened powershell as Admin."
+    [Environment]::Exit(1)
+}
+
+Write-Host "iris installation is successful!"
