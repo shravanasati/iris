cd9ec2d4dcf17c3a00c40434559b53df342175ed
Shravan Asati <dev.shravan@protonmail.com>
2021-09-24 22:40:18 +0530

    ⚡️ verbosity and executable strip in build script

--- scripts/build.py
+++ scripts/build.py
@@ -5,7 +5,10 @@ import shlex
 import shutil
 import os
 
-app_name = "iris"
+# build config
+APP_NAME = "iris"
+STRIP = True
+VERBOSE = False
 
 
 def init_folders() -> None:
@@ -21,48 +24,78 @@ def init_folders() -> None:
 
 def pack(dir: str, platform: str) -> None:
     """
-    Creates a tarball file for the given directory.
+    Copies README, LICENSE, CHANGELOG and iris logo to the output directory and creates a tarball file for the given platform.
     """
     shutil.copyfile("./README.md", f"{dir}/README.md")
-    shutil.copyfile("./LICENSE", f"{dir}/LICENSE")
+    shutil.copyfile("./LICENSE.txt", f"{dir}/LICENSE.txt")
     shutil.copyfile("./CHANGELOG.md", f"{dir}/CHANGELOG.md")
     shutil.copyfile("./assets/gopher.png", f"{dir}/iris.png")
 
-    build_os = platform.split("/")[0]
-    build_arch = platform.split("/")[1]
+    splitted = platform.split("/")
+    build_os = splitted[0]
+    build_arch = splitted[1]
 
-    shutil.make_archive(f"dist/{app_name}_{build_os}_{build_arch}", "gztar", dir)
+    shutil.make_archive(f"dist/{APP_NAME}_{build_os}_{build_arch}", "gztar", dir)
 
 
 def build(platform: str) -> None:
+    """
+    Calls the go compiler to build the application for the given platform, and the pack function.
+    """
     print(f"==> Building for {platform}.")
     splitted = platform.split("/")
     build_os = splitted[0]
     build_arch = splitted[1]
 
-    output_dir = f"temp/{app_name}_{build_os}_{build_arch}"
+    output_dir = f"temp/{APP_NAME}_{build_os}_{build_arch}"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    executable_path = f"{output_dir}/{app_name}"
+    executable_path = f"{output_dir}/{APP_NAME}"
     if build_os == "windows":
         executable_path += ".exe"
 
     os.environ["GOOS"] = build_os
     os.environ["GOARCH"] = build_arch
 
-    run(shlex.split(f"go build -o {executable_path}"), check=True)
+    run(
+        shlex.split(
+            "go build -o {} {} {}".format(
+                executable_path,
+                '-ldflags="-s -w"' if STRIP else "",
+                "-v" if VERBOSE else "",
+            )
+        ),
+        check=True,
+    )
 
     print(f"==> Packing for {platform}.")
     pack(output_dir, platform)
 
 
-if __name__ == "__main__":
-    platforms: List[str] = ["linux/amd64", "windows/amd64", "darwin/amd64", "darwin/arm64", "linux/arm64"]
-    init_folders()
-
-    with Pool(processes=cpu_count()) as pool:
-        pool.map(build, platforms)
-
+def cleanup() -> None:
+    """
+    Removes the `temp` folder.
+    """
     print("==> Cleaning up.")
     shutil.rmtree("./temp")
+
+
+if __name__ == "__main__":
+    platforms: List[str] = [
+        "linux/amd64",
+        "windows/amd64",
+        "darwin/amd64",
+        "darwin/arm64",
+        "linux/arm64",
+    ]
+
+    init_folders()
+
+    max_procs = cpu_count()
+    print(f"==> Starting builds with {max_procs} processes.")
+
+    with Pool(processes=max_procs) as pool:
+        pool.map(build, platforms)
+
+    cleanup()
